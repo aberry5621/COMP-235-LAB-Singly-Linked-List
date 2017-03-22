fb72aa106e57e8a470b3d2489cb28030a8f5174a
gb_man <gb_man@gb-mans-iMac.local>
2017-03-22 14:42:29 -0700

    Initial Commit

---
+++ COMP 235 LAB Singly Linked List/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP 235 LAB Singly Linked List
+//
+//  Created by gb_man on 3/22/17.
+//  Copyright © 2017 alex. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
